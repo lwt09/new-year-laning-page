58acd587d12177b653b9a2a3d1823ec7b835e392
lwt09 <1102757555@qq.com>
2024-01-22 10:39:19 +0800

    wip: dialog-step1

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -13,20 +13,28 @@
           <div class="content-info">
             <div class="row">
               <span class="label">昵称</span>
-              <input type="text" />
+              <input v-model="name" maxlength="6" class="name_input" type="text" placeholder="请输入昵称                6" />
             </div>
             <div class="row">
               <span class="label">性别</span>
-              <input type="checkbox" /><span>男</span>
-              <input type="checkbox" /><span>女</span>
+              <div class="custom-ridio" @click="sex = 'male'">
+                <img class="radio-icon" v-if="sex === 'male'" src="@assets/image/checked.png" />
+                <img class="radio-icon" v-if="sex === 'female'" src="@assets/image/uncheck.png" />
+                <span class="ridio-icon-text">男</span>
+              </div>
+              <div class="custom-ridio" @click="sex = 'female'">
+                <img class="radio-icon" v-if="sex === 'male'" src="@assets/image/uncheck.png" />
+                <img class="radio-icon" v-if="sex === 'female'" src="@assets/image/checked.png" />
+                <span class="ridio-icon-text">女</span>
+              </div>
             </div>
           </div>
         </template>
       </div>
       <div class="footer-box">
         <slot name="footer">
-          <div class="back"><img src="@assets/image/back.png" /></div>
-          <div class="next"><img src="@assets/image/next.png" /></div>
+          <div class="next" @click="nextStep"><img src="@assets/image/next.png" /></div>
+          <div class="back" ><img src="@assets/image/back.png" /></div>
         </slot>
       </div>
       <div class="close-box" @click="closeModal">
@@ -66,6 +74,19 @@ function closeModal() {
 }
 
 const step = ref(1);
+// step1
+const sex = ref("male");
+const name = ref("");
+
+function nextStep() {
+  if (step.value === 1) {
+    if (!name.value) {
+      alert("请先输入昵称哦！");
+      return
+    }
+    step.value = 2;
+  }
+}
 </script>
 
 <style scoped lang="scss">
@@ -138,8 +159,32 @@ const step = ref(1);
     padding-top: 30px;
     .row {
       display: flex;
+      align-items: center;
+      margin-bottom: 10px;
       .label {
+        margin-right: 10px;
         flex-shrink: 0;
+        color: #f3ddbd;
+      }
+      .name_input {
+        width: 80%;
+        border-radius: 7px;
+        background-color: #ffe3cc;
+        border: none;
+        padding: 5px;
+      }
+      .name_input::placeholder {
+        font-size: 12px; /* 设置placeholder的字体大小 */
+        color: #b6885f; /* 这里还可以设置placeholder的文本颜色 */
+      }
+
+      .custom-ridio {
+        display: flex;
+        align-items: center;
+        .radio-icon {
+          margin: 0 10px;
+          width: 15px;
+        }
       }
     }
   }
@@ -152,6 +197,7 @@ const step = ref(1);
   flex-shrink: 0;
   margin-top: 40px;
   justify-content: space-between;
+  flex-direction: row-reverse;
   .back {
     width: 45%;
     height: auto;
